875a92f0c7852b39b70235f85b6256aa32603ffe
Rikstone <tarikbouari@gmail.com>
2022-09-14 16:47:46 +0000

    update file

--- src/components/component.test.js
+++ src/components/component.test.js
@@ -1,21 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import Quotes from './Quotes';
-import Welcome from './Welcome';
-import Calculator from './Calculator';
+import { render, screen } from "@testing-library/react";
+import Quotes from "./Quotes";
+import Welcome from "./Welcome";
+import Calculator from "./Calculator";
 
-describe(' testing components', () => {
-  test('testing landing page', () => {
+describe(" testing components", () => {
+  test("testing landing page", () => {
     const landing = render(<Welcome />);
     expect(landing).toMatchSnapshot();
   });
-  test('testing Quote Page', async () => {
-     render(<Quotes />);
-    const button = await screen.findAllByRole('button');
+  test("testing Quote Page", async () => {
+    render(<Quotes />);
+    const button = await screen.findAllByRole("button");
     expect(button).toHaveLength(1);
   });
-  test('testing calculator page', async ()=>{
+  test("testing calculator page", async () => {
     render(<Calculator />);
-    const btn = await screen.findAllByRole('button');
-    expect(btn).toHaveLength(19)
+    const btn = await screen.findAllByRole("button");
+    expect(btn).toHaveLength(19);
   });
 });
